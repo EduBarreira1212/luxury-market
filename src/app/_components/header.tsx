14e76ea5ecd104bbc89f995a440b6704e07ea5b1
Eduardo Barreira <edubarreira1212@gmail.com>
2025-03-04 22:13:16 -0300

    feat: create Header component

---
+++ src/app/_components/header.tsx
@@ -0,0 +1,27 @@
+import HeaderRight from './header-right';
+import NavbarPortal from './navbar-portal';
+import { Input } from './ui/input';
+
+type HeaderProps = {
+    searchBarExists: boolean;
+    variant: 'black' | 'white';
+};
+
+const Header = ({ searchBarExists, variant }: HeaderProps) => {
+    return (
+        <div className="flex w-full flex-row items-center justify-between px-5">
+            <div
+                className={`flex items-center gap-3 ${variant == 'black' ? 'text-black' : 'text-white'}`}
+            >
+                <NavbarPortal />
+                <h1 className="text-xl">Luxury Market</h1>
+            </div>
+            {searchBarExists && (
+                <Input className="w-[35%] rounded-2xl" placeholder="Search" />
+            )}
+            <HeaderRight textColor={variant} />
+        </div>
+    );
+};
+
+export default Header;
