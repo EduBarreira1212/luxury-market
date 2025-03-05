3505bbb3e49b66f5ca4a5f46ba7f02ae3da714c0
Eduardo Barreira <edubarreira1212@gmail.com>
2025-03-04 22:12:43 -0300

    feat: add textColor prop to HeaderRight component for dynamic styling

--- src/app/_components/header-right.tsx
+++ src/app/_components/header-right.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { Button } from './ui/button';
 import { User2 } from 'lucide-react';
 
-const HeaderRight = () => {
+type HeaderRightProp = {
+    textColor: 'black' | 'white';
+};
+
+const HeaderRight = ({ textColor }: HeaderRightProp) => {
     return (
         <div className="flex items-center gap-2">
-            <Button variant="link" className="text-base text-white">
+            <Button variant="link" className={`text-base text-${textColor}`}>
                 Sell with us
             </Button>
             <Button variant="outline" className="items-center rounded-xl text-base">
