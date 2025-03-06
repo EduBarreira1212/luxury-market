3e6a1fbc0ade0b4708bc77374c43673fc1d7d0f8
Eduardo Barreira <edubarreira1212@gmail.com>
2025-03-06 19:14:13 -0300

    feat: create initial version of CreateSellerPage

---
+++ src/app/seller/page.tsx
@@ -0,0 +1,13 @@
+import Header from '../_components/header';
+import CreateSellerForm from './_components/create-seller-form';
+
+const CreateSellerPage = () => {
+    return (
+        <div className="h-screen w-screen">
+            <Header searchBarExists={false} variant="black" />
+            <CreateSellerForm />
+        </div>
+    );
+};
+
+export default CreateSellerPage;
