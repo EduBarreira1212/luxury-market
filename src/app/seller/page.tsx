9073d00cb5a2ba76fd15349423eec0d720fd1145
Eduardo Barreira <edubarreira1212@gmail.com>
2025-03-06 19:22:04 -0300

    fix: adjust width of container in CreateSellerPage

--- src/app/seller/page.tsx
+++ src/app/seller/page.tsx
@@ -3,7 +3,7 @@ import CreateSellerForm from './_components/create-seller-form';
 
 const CreateSellerPage = () => {
     return (
-        <div className="h-screen w-screen">
+        <div className="h-screen w-full">
             <Header searchBarExists={false} variant="black" />
             <CreateSellerForm />
         </div>
