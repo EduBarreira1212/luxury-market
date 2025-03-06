5c8679521a23c53b47f911da45feb61c86a61ec7
Eduardo Barreira <edubarreira1212@gmail.com>
2025-03-06 18:43:16 -0300

    fix: remove unnecessary padding in Car and Motorcycle details pages

--- src/app/cars/[id]/page.tsx
+++ src/app/cars/[id]/page.tsx
@@ -19,50 +19,54 @@ const CarDetailsPage = async ({ params: { id } }: { params: Params }) => {
     if (!seller) return;
 
     return (
-        <div className="flex w-full flex-col items-center gap-4 p-3">
+        <div className="flex w-full flex-col items-center gap-4">
             <Header searchBarExists={false} variant="black" />
-            <div className="relative h-44 w-2/5">
-                <Image
-                    src="/images/fallback.png"
-                    alt="car"
-                    fill
-                    className="object-cover"
-                />
-            </div>
-            <div className="flex w-2/5 flex-col gap-4">
-                <div className="flex w-full justify-between">
-                    <h1 className="text-2xl">
-                        {car?.brand} {car?.model}
-                    </h1>
-                    <span className="text-2xl">{formatCurrency(car!.price)}</span>
+            <div className="flex w-full flex-col items-center border-t-2 py-3">
+                <div className="relative h-44 w-2/5">
+                    <Image
+                        src="/images/fallback.png"
+                        alt="car"
+                        fill
+                        className="object-cover"
+                    />
                 </div>
-                <div className="flex gap-4 border-y-2 border-black py-3">
-                    <div className="flex flex-col">
-                        <span>{car?.year}</span>
-                        <span className="text-slate-500">Year</span>
+                <div className="flex w-2/5 flex-col gap-4">
+                    <div className="flex w-full justify-between">
+                        <h1 className="text-2xl">
+                            {car?.brand} {car?.model}
+                        </h1>
+                        <span className="text-2xl">
+                            {formatCurrency(car!.price)}
+                        </span>
                     </div>
-                    <div className="flex flex-col">
-                        <span>{car?.mileage.toLocaleString()}</span>
-                        <span className="text-slate-500">Mileage</span>
+                    <div className="flex gap-4 border-y-2 border-black py-3">
+                        <div className="flex flex-col">
+                            <span>{car?.year}</span>
+                            <span className="text-slate-500">Year</span>
+                        </div>
+                        <div className="flex flex-col">
+                            <span>{car?.mileage.toLocaleString()}</span>
+                            <span className="text-slate-500">Mileage</span>
+                        </div>
+                        <div className="flex flex-col">
+                            <span>{car?.condition}</span>
+                            <span className="text-slate-500">Condition</span>
+                        </div>
+                        <div className="flex flex-col">
+                            <span>{car?.gearbox}</span>
+                            <span className="text-slate-500">Gearbox</span>
+                        </div>
+                        <div className="flex flex-col">
+                            <span>{car?.fuel}</span>
+                            <span className="text-slate-500">Fuel type</span>
+                        </div>
                     </div>
-                    <div className="flex flex-col">
-                        <span>{car?.condition}</span>
-                        <span className="text-slate-500">Condition</span>
+                    <div>
+                        <h2 className="mb-2 text-xl">About this car</h2>
+                        <span>{car?.about}</span>
                     </div>
-                    <div className="flex flex-col">
-                        <span>{car?.gearbox}</span>
-                        <span className="text-slate-500">Gearbox</span>
-                    </div>
-                    <div className="flex flex-col">
-                        <span>{car?.fuel}</span>
-                        <span className="text-slate-500">Fuel type</span>
-                    </div>
+                    <SellerInfo seller={seller} />
                 </div>
-                <div>
-                    <h2 className="mb-2 text-xl">About this car</h2>
-                    <span>{car?.about}</span>
-                </div>
-                <SellerInfo seller={seller} />
             </div>
         </div>
     );
