80a85743c3c0e37274855dd041bc2a6a61b28e99
Eduardo Barreira <edubarreira1212@gmail.com>
2025-03-10 12:30:53 -0300

    chore: add Credentials config

--- src/app/_lib/auth.ts
+++ src/app/_lib/auth.ts
@@ -1,5 +1,20 @@
 import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [],
+    providers: [
+        Credentials({
+            credentials: {
+                email: {},
+                password: {},
+            },
+            authorize: async (credentials) => {
+                // logic to salt and hash password
+                // logic to verify if the user exists
+                // No user found, so this is their first attempt to login
+                // Optionally, this is also the place you could do a user registration
+                // return user object with their profile data
+            },
+        }),
+    ],
 });
