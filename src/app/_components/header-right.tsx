0b829ccf321b1bb3e1e561f14e14f1de940e5405
Eduardo Barreira <edubarreira1212@gmail.com>
2025-03-17 22:04:20 -0300

    feat: integrate AccountDropdown for authenticated users in header

--- src/app/_components/header-right.tsx
+++ src/app/_components/header-right.tsx
@@ -2,18 +2,29 @@ import React from 'react';
 import { Button } from './ui/button';
 import Link from 'next/link';
 import LogInDialog from './log-in-dialog';
+import { auth } from '../_lib/auth';
+import AccountDropdown from './account-dropdown';
 
 type HeaderRightProp = {
     textColor: 'black' | 'white';
 };
 
-const HeaderRight = ({ textColor }: HeaderRightProp) => {
+const HeaderRight = async ({ textColor }: HeaderRightProp) => {
+    const session = await auth();
+
     return (
         <div className="flex items-center gap-2">
             <Button variant="link" className={`text-base text-${textColor}`} asChild>
                 <Link href="/seller">Sell with us</Link>
             </Button>
-            <LogInDialog />
+            {session?.user ? (
+                <AccountDropdown
+                    textColor={textColor}
+                    username={session.user.name!}
+                />
+            ) : (
+                <LogInDialog />
+            )}
         </div>
     );
 };
