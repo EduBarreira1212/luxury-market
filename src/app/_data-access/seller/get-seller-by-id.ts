45332c7d329f716764b04cc165643e61127a1dad
Eduardo Barreira <edubarreira1212@gmail.com>
2025-03-18 21:06:59 -0300

    feat: add DTO to getSellerById function and omit password

--- src/app/_data-access/seller/get-seller-by-id.ts
+++ src/app/_data-access/seller/get-seller-by-id.ts
@@ -1,8 +1,26 @@
 import { db } from '@/app/_lib/prisma';
 import 'server-only';
 
-export const getSellerById = async (id: string) => {
-    const seller = await db.seller.findUnique({ where: { id } });
+export type SellerResponseDTO = {
+    id: string;
+    name: string;
+    email: string;
+    address: string;
+    phoneNumber: string;
+    about: string;
+    createdAt: Date;
+    updatedAt: Date;
+};
+
+export const getSellerById = async (
+    id: string,
+): Promise<SellerResponseDTO | null> => {
+    const seller = await db.seller.findUnique({
+        where: { id },
+        omit: {
+            password: true,
+        },
+    });
 
     return seller;
 };
