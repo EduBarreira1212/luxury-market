4a24fb81109fe7a47da6e21951a105ed16ed06fd
Eduardo Barreira <edubarreira1212@gmail.com>
2025-03-19 23:30:49 -0300

    feat: create server action to update a seller

---
+++ src/app/_actions/update-seller.ts
@@ -0,0 +1,29 @@
+'use server';
+
+import { db } from '../_lib/prisma';
+
+type UpdateSellerParams = {
+    name: string;
+    email: string;
+    address: string;
+    phoneNumber: string;
+    about: string;
+};
+
+export const updateSeller = async (
+    id: string,
+    updateSellerParams: UpdateSellerParams,
+) => {
+    try {
+        await db.seller.update({
+            where: { id },
+            data: updateSellerParams,
+            omit: {
+                password: true,
+            },
+        });
+    } catch (error) {
+        console.error('Error updating seller:', error);
+        throw new Error('Failed to update seller.');
+    }
+};
