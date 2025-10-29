f8236f15c05af55bb78f99d629d0549d49b2c9f1
Eduardo Barreira <edubarreira1212@gmail.com>
2025-10-29 19:00:59 -0300

    fix: ensure footer stays at bottom

--- src/app/seller/page.tsx
+++ src/app/seller/page.tsx
@@ -3,7 +3,7 @@ import CreateSellerForm from './_components/create-seller-form';
 
 const CreateSellerPage = () => {
     return (
-        <div className="flex w-full flex-col items-center gap-4">
+        <div className="flex h-screen w-full flex-col items-center gap-4">
             <Header searchBarExists={false} variant="black" />
             <div className="flex w-full flex-col items-center gap-4 border-t-2 py-3">
                 <h1 className="text-3xl">Create your seller account</h1>
