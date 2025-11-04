056a3d3ea65ecc7fbfd1b03ad541d4944f90d160
Eduardo Barreira <edubarreira1212@gmail.com>
2025-11-04 17:58:47 -0300

    feat: update createBuyer function to hash password and use SignUpBuyerFormValues

--- src/app/_actions/create-buyer.ts
+++ src/app/_actions/create-buyer.ts
@@ -1,11 +1,18 @@
 'use server';
 
+import bcrypt from 'bcryptjs';
 import { db } from '../_lib/prisma';
-import { BuyerFormValues } from '../buyer/_components/create-buyer-form';
+import { SignUpBuyerFormValues } from '../_schemas/sign-up-buyer';
 
-export const createBuyer = async (createBuyerParams: BuyerFormValues) => {
+export const createBuyer = async (createBuyerParams: SignUpBuyerFormValues) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, confirmPassword: _, ...otherParams } = createBuyerParams;
     try {
-        await db.buyer.create({ data: createBuyerParams });
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        await db.buyer.create({
+            data: { ...otherParams, password: hashedPassword },
+        });
     } catch (error) {
         console.error('Error creating buyer:', error);
         throw new Error('Failed to create buyer.');
