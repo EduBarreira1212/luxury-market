549cbfbb3ed748c26df8eb20b8e8fb803a1961b6
Eduardo Barreira <edubarreira1212@gmail.com>
2025-11-05 23:13:32 -0300

    feat: add TypeScript definitions for NextAuth session and JWT with user roles

---
+++ src/app/_types/next-auth.d.ts
@@ -0,0 +1,17 @@
+import NextAuth, { DefaultSession } from 'next-auth';
+
+declare module 'next-auth' {
+    interface Session {
+        user: {
+            id: string;
+            role: 'buyer' | 'seller';
+        } & DefaultSession['user'];
+    }
+}
+
+declare module 'next-auth/jwt' {
+    interface JWT {
+        id: string;
+        role: 'buyer' | 'seller';
+    }
+}
