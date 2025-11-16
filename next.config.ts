8c2683edf204c9c730938725f3906937f14743e1
Eduardo Barreira <edubarreira1212@gmail.com>
2025-11-15 23:21:16 -0300

    chore: configure remote image patterns for S3 integration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'd16z39zkx8cl7p.cloudfront.net',
+                pathname: '/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
